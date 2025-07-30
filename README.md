9124145ea70b05025fcc19c5ef06ebf9d75fac05
kimuralj <73235962+kimuralj@users.noreply.github.com>
2025-07-30 20:16:13 -0300

    Committing initial files

---
+++ README.md
@@ -0,0 +1,12 @@
+## Overview
+
+This project aims at reading the data of a Time-of-Flight VL53L5CX sensor from STMicroelectronics. The sensor is connected through I2C and the data is sent via serial.
+
+### Hardware Required
+
+Two basic things are required for this project to run: a VL53L5CX ToF sensor and an ESP32-C3 (it could work with other ESP32 models as well).
+
+
+### Build and Flash
+
+Enter `idf.py -p PORT flash monitor` to build, flash and monitor the project.
---
+++ main/main.c
@@ -0,0 +1,128 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "vl53l5cx_api.h"
+
+static const char *TAG = "VL53L5CX_Serial";
+
+void app_main(void)
+{
+    //Define the i2c bus configuration
+    i2c_port_t i2c_port = I2C_NUM_0;
+    i2c_config_t i2c_config = {
+            .mode = I2C_MODE_MASTER,
+            .sda_io_num = 5,
+            .scl_io_num = 4,
+            .sda_pullup_en = GPIO_PULLUP_ENABLE,
+            .scl_pullup_en = GPIO_PULLUP_ENABLE,
+            .master.clk_speed = VL53L5CX_MAX_CLK_SPEED,
+    };
+
+    i2c_param_config(i2c_port, &i2c_config);
+    i2c_driver_install(i2c_port, i2c_config.mode, 0, 0, 0);
+
+    /*********************************/
+    /*   VL53L5CX ranging variables  */
+    /*********************************/
+
+    uint8_t 				status, loop, isAlive, isReady;
+    VL53L5CX_Configuration 	Dev;			/* Sensor configuration */
+    VL53L5CX_ResultsData 	Results;		/* Results data from VL53L5CX */
+
+
+    /*********************************/
+    /*      Customer platform        */
+    /*********************************/
+
+    /* Fill the platform structure with customer's implementation. For this
+    * example, only the I2C address is used.
+    */
+    Dev.platform.address = VL53L5CX_DEFAULT_I2C_ADDRESS;
+    Dev.platform.port = i2c_port;
+
+    /* (Optional) Reset sensor toggling PINs (see platform, not in API) */
+    //Reset_Sensor(&(Dev.platform));
+
+    /* (Optional) Set a new I2C address if the wanted address is different
+    * from the default one (filled with 0x20 for this example).
+    */
+    //status = vl53l5cx_set_i2c_address(&Dev, 0x20);
+
+
+    /*********************************/
+    /*   Power on sensor and init    */
+    /*********************************/
+
+    /* (Optional) Check if there is a VL53L5CX sensor connected */
+    status = vl53l5cx_is_alive(&Dev, &isAlive);
+    if(!isAlive || status)
+    {
+        printf("VL53L5CX not detected at requested address\n");
+        return;
+    }
+
+    /* (Mandatory) Init VL53L5CX sensor */
+    status = vl53l5cx_init(&Dev);
+    if(status)
+    {
+        printf("VL53L5CX ULD Loading failed\n");
+        return;
+    }
+
+    printf("VL53L5CX ULD ready ! (Version : %s)\n",
+           VL53L5CX_API_REVISION);
+
+    vl53l5cx_set_resolution(&Dev, VL53L5CX_RESOLUTION_8X8);
+    vl53l5cx_set_ranging_frequency_hz(&Dev, 5);
+    vl53l5cx_set_ranging_mode(&Dev, VL53L5CX_RANGING_MODE_CONTINUOUS);
+    vl53l5cx_set_sharpener_percent(&Dev, 20);
+
+    
+    /*********************************/
+    /*         Ranging loop          */
+    /*********************************/
+
+    status = vl53l5cx_start_ranging(&Dev);
+
+    loop = 0;
+    while(loop < 100)
+    {
+        /* Use polling function to know when a new measurement is ready.
+         * Another way can be to wait for HW interrupt raised on PIN A1
+         * (INT) when a new measurement is ready */
+
+        status = vl53l5cx_check_data_ready(&Dev, &isReady);
+
+        if(isReady)
+        {
+            vl53l5cx_get_ranging_data(&Dev, &Results);
+
+            /* As the sensor is set in 4x4 mode by default, we have a total
+             * of 16 zones to print. For this example, only the data of first zone are
+             * print */
+            printf("Print data no : %3u\n", Dev.streamcount);
+            for(int i = 0; i < 64; i+=8)
+            {
+                printf("%d,%d,%d,%d,%d,%d,%d,%d\n",
+                    Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*i],
+                    Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*(i+1)],
+                    Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*(i+2)],
+                    Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*(i+3)],
+                    Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*(i+4)],
+                    Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*(i+5)],
+                    Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*(i+6)],
+                    Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*(i+7)]);
+            }
+            // printf("\n");
+            // loop++;
+        }
+
+        /* Wait a few ms to avoid too high polling (function in platform
+         * file, not in API) */
+        WaitMs(&(Dev.platform), 5);
+    }
+
+    // Finish execution
+    status = vl53l5cx_stop_ranging(&Dev);
+    printf("End of ULD demo\n");
+}
